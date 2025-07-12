b5ddda1d53a9b74599e89f698cbb820c082ce915
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-07-12 23:28:23 +0530

    Add routing and new pages for Home, About, Contact, Doctors, Login, My Appointment, and My Profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <div className="text-3xl font-bold underline text-green-600">App</div>
+    <div>App</div>
   )
 }
+
+export default App
