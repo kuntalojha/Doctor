1bfc65c29daa441270ca1b84b6767557fc727e5d
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-07-13 22:17:09 +0530

    Fix deployment script and update base path for Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Doctor/', // Set the base path for deployment
   plugins: [react(), tailwindcss()],
-})
+});
