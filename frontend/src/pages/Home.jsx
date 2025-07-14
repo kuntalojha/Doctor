9946b6863b37fcd9443667dab700f149a90f01c7
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-07-14 23:08:57 +0530

    Add TopDoctors component and integrate it into Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,14 @@ import React from 'react';
 // Importing components
 import Header from '../components/Header';
 import SpecialityMenu from '../components/SpecialityMenu';
+import TopDoctors from '../components/TopDoctors';
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <TopDoctors />
       <h1>Welcome to the Home Page</h1>
     </div>
   );
